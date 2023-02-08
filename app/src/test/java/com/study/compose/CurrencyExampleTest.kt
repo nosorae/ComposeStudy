814f24024853a90faf546102266b31dd885910ed
nosorae <shthfo@gmail.com>
2023-02-09 01:18:25 +0900

    feat: 2장 실패

--- app/src/test/java/com/study/compose/CurrencyExampleTest.kt
+++ app/src/test/java/com/study/compose/CurrencyExampleTest.kt
@@ -9,5 +9,7 @@ class CurrencyExampleTest {
         val five = Dollor(5)
         five.times(2)
         assert(10 == five.amount)
+        five.times(3)
+        assert(15 == five.amount)
     }
 }
