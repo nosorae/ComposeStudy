493567269988715059f8cb0f6e652fc2d86d7110
nosorae <shthfo@gmail.com>
2023-02-23 01:10:58 +0900

    feat: 18장 원시테스트 (testMethod 호출하면 true)

--- app/src/main/java/com/study/compose/tdd/xUnit.kt
+++ app/src/main/java/com/study/compose/tdd/xUnit.kt
@@ -1,7 +1,16 @@
 package com.study.compose.tdd
 
-class WasRun(var wasRun: Boolean = false, name: String) {
+import kotlin.reflect.full.functions
+
+class WasRun(var wasRun: Boolean = false, val name: String) {
     fun testMethod() {
         wasRun = true
+    }
+
+    operator fun invoke() {
+        this::class.java.methods.find { func ->
+            println("func ${func.name}")
+            func.name == name
+        }?.invoke(Unit)
     }
 }
--- app/src/test/java/com/study/compose/XUnitTest.kt
+++ app/src/test/java/com/study/compose/XUnitTest.kt
@@ -1,10 +1,12 @@
 package com.study.compose
 
 import com.study.compose.tdd.WasRun
+import org.junit.Ignore
 import org.junit.Test
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
+@Ignore
 class XUnitTest {
     @Test
     fun testWasRun() {
